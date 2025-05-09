924084db7008cf6b7390bf497c20fd699dd6f5d1
ThomasDosio <thomas.e.dosio@gmail.com>
2025-05-09 17:04:11 +0100

    Added a bunch of stuff

--- src/EVEMain.java
+++ src/EVEMain.java
@@ -2,46 +2,71 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class EVEMain {
+public class EVEMain
+    {
         public static void main(String[] args)
             {
 
                 Scanner keyboard = new Scanner(System.in);
 
+                //makes list of vehicles ot order them by id
+                ArrayList<ArrayList<ArrayList<String>>> vehicles =
+                        new ArrayList<>();
 
-                {
-                    //makes list of vehicles ot order them by id
-                    ArrayList<ArrayList<ArrayList<String>>> vehicles = new ArrayList<>();
+                while (keyboard.hasNext() && (keyboard.hasNext("vehicle_id") ||
+                        keyboard.hasNext("vehicle_id:")))
+                    {
+                        ArrayList<ArrayList<String>> vehicle =
+                                CorrectInput(keyboard);
 
-                    while (keyboard.hasNext() && (keyboard.hasNext("vehicle_id")||keyboard.hasNext("vehicle_id:")))
-                        {
-                            ArrayList<ArrayList<String>> vehicle =
-                                    CorrectInput(keyboard);
-                            if(vehicles.isEmpty()) vehicles.add(vehicle);
-                            else for (int k = 0; k < vehicles.size(); k++)
-                                {
-                                    if (Integer.parseInt(vehicles.get(k).get(0).get(1))>Integer.parseInt(vehicle.get(0).get(1)))
-                                        {
-                                            vehicles.add(k, vehicle);
-                                            break;
-                                        }
+                        if (vehicles.isEmpty())
+                            {
+                                vehicles.add(vehicle);
+                            } else
+                            {
+                                for (int k = 0; k < vehicles.size(); k++)
+                                    {
+                                        if (Integer.parseInt(
+                                                vehicles.get(k).get(0).get(1)) >
+                                                Integer.parseInt(
+                                                        vehicle.get(0).get(1)))
+                                            {
+                                                vehicles.add(k, vehicle);
+                                                break;
+                                            } else if (
+                                                k == vehicles.size() - 1 &&
+                                                        Integer.parseInt(
+                                                                vehicles.get(k)
+                                                                        .get(0)
+                                                                        .get(1)) <=
+                                                                Integer.parseInt(
+                                                                        vehicle.get(
+                                                                                        0)
+                                                                                .get(1)))
+                                            {
+                                                vehicles.add(vehicle);
+                                            } else if (Integer.parseInt(
+                                                vehicles.get(k).get(0)
+                                                        .get(1)) <=
+                                                Integer.parseInt(
+                                                        vehicle.get(0).get(1)))
+                                            {
+                                                vehicles.add(vehicle);
+                                            }
+                                    }
+                            }
+                    }
 
-                                    else if (k == vehicles.size()-1 && Integer.parseInt(vehicles.get(k).getFirst().get(1))<=Integer.parseInt(vehicle.getFirst().get(1)))
-                                        {
-                                            vehicles.add(vehicle);
-                                        }
-                                }
-                        }
+                for (ArrayList<ArrayList<String>> vehicle : vehicles)
+                    {
+                        PrettyPrint(vehicle);
+                        System.out.println();
+                    }
 
-                    for (ArrayList<ArrayList<String>> arrayLists : vehicles)
-                        {
-                            PrettyPrint(arrayLists);
-                            System.out.println();
-                        }
-                }
             }
 
-        public static ArrayList<ArrayList<String>> CorrectInput (Scanner keyboard)
+        public static ArrayList<ArrayList<String>> CorrectInput(
+                Scanner keyboard)
             {
                 if (keyboard.hasNext())
                     {
@@ -49,20 +74,20 @@ public class EVEMain {
                         ArrayList<ArrayList<String>> vehicle =
                                 new ArrayList<>();
 
+                        boolean allValidKeys= true;
+
                         //for each of the six categories it adds
                         // the key and the info to a list
                         for (int j = 0; j < 6; j++)
                             {
                                 // creates a list with key and
                                 // information
-                                ArrayList<String> info =
-                                        new ArrayList<>();
+                                ArrayList<String> info = new ArrayList<>();
 
                                 String key = keyboard.next();
                                 String string = keyboard.nextLine();
 
-                                String [] information =
-                                        string.split(",");
+                                String[] information = string.split(",");
 
                                 for (String s : information)
                                     {
@@ -75,10 +100,37 @@ public class EVEMain {
                                                 key.indexOf(":"));
                                     }
 
-                                info.addFirst(key);
+                                String [] Keys = {"vehicle_id","type", "color",
+                                        "manufacturer", "engine", "wheels"};
+                                boolean validKeys = false;
+
+                                for (String s : Keys)
+                                    {
+                                        if (key.equals(s))
+                                            {
+                                                validKeys = true;
+                                                break;
+                                            }
+                                    }
+
+                                if (validKeys) info.add(0, key);
+                                else allValidKeys = false;
 
                                 vehicle.add(info);
                             }
+
+                        ArrayList<String> cost = new ArrayList<>();
+                        cost.add("cost");
+                        cost.add("0");
+                        vehicle.add(cost);
+
+                        Sort(vehicle);
+
+                        ArrayList<String> valid = new ArrayList<>();
+                        valid.add("valid");
+                        if (validation(vehicle) && allValidKeys) valid.add(1,"true");
+                        else valid.add(1 ,"false");
+                        vehicle.add(valid);
 
                         Sort(vehicle);
 
@@ -89,41 +141,127 @@ public class EVEMain {
                 return null;
             }
 
-        public static void PrettyPrint (ArrayList<ArrayList<String>> vehicle)
+        public static void PrettyPrint(ArrayList<ArrayList<String>> vehicle)
             {
                 for (ArrayList<String> info : vehicle)
                     {
                         System.out.println();
-                        System.out.print(info.getFirst().trim() + ": ");
+                        System.out.print(info.get(0).trim() + ": ");
                         for (int i = 1; i < info.size(); i++)
                             {
-                                System.out.print(removeColon(removeZero(info.get(i).trim())).trim());
-                                if(i != info.size() - 1) System.out.print(", ");
+                                System.out.print(removeColon(
+                                        removeZero(info.get(i).trim())).trim());
+                                if (i != info.size() - 1)
+                                    {
+                                        System.out.print(", ");
+                                    }
                             }
                     }
             }
 
+        public static ArrayList<ArrayList<String>> FixFormatting(ArrayList<ArrayList<String>> vehicle)
+            {
+                for (ArrayList<String> info : vehicle)
+                    {
+                        for (int i = 1; i < info.size(); i++)
+                            {
+                                removeColon(removeZero(info.get(i).trim()));
+                            }
+                    }
+
+                return vehicle;
+            }
+
         public static String removeZero(String str)
             {
+                if (str.equals("0"))
+                    {
+                        return str;
+                    }
+
                 int i = 0;
-                while (i < str.length() && str.charAt(i) == '0') i++;
+                while (i < str.length() && str.charAt(i) == '0')
+                    {
+                        i++;
+                    }
                 return str.substring(i);
+
             }
 
         public static String removeColon(String str)
             {
                 int i = 0;
-                while (i < str.length() && str.charAt(i) == ':') i++;
-                return str.substring(i);
+                while (i < str.length() && str.charAt(i) == ':')
+                    {
+                        i++;
+                    }
+                return str.substring(i).trim();
             }
 
-        public static void Sort (ArrayList<ArrayList<String>> list)
+        public static void Sort(ArrayList<ArrayList<String>> list)
             {
-                ArrayList<String> id = list.getFirst();
-                list.sort(Comparator.comparing(ArrayList::getFirst));
+                ArrayList<String> id = list.get(0);
+                list.sort(Comparator.comparing(o -> o.get(0).trim()));
                 list.remove(id);
-                list.addFirst(id);
+                list.add(0, id);
             }
+
+        public static boolean validation(ArrayList<ArrayList<String>> vehicle)
+            {
+                String[] colors =
+                        {"red", "green", "blue", "orange", "yellow", "purple",
+                                "pink", "black", "white", "silver", "gold"};
+                boolean validColours = isEqual(vehicle, 1, 1, colors);
+                String[] manufacturers =
+                        {"ACME", "Consolidated Products", "Goliath Inc."};
+                boolean validManufacturers =
+                        isEqual(vehicle, 4, 1, manufacturers) &&
+                                isEqual(vehicle, 3, 1, manufacturers);
+                String[] types = {"car", "truck", "motorcycle"};
+                boolean validTypes = isEqual(vehicle, 5, 1, types);
+                String[] carSubtypes = {"hatchback", "sedan", "convertible"};
+                String[] truckSubtypes = {"pickup", "eighteen wheeler"};
+                String[] motorcycleSubtypes = {"sport", "touring"};
+                String[] fuels = {"diesel", "petrol", "electric"};
+                String[] truckfuels = {"diesel", "petrol"};
+                boolean validSubtypes = (vehicle.get(5).get(1).equals("car") &&
+                        isEqual(vehicle, 5, 2, carSubtypes)) ||
+                        (vehicle.get(5).get(1).equals("truck") &&
+                                isEqual(vehicle, 5, 2, truckSubtypes)) ||
+                        (vehicle.get(5).get(1).equals("motorcycle") &&
+                                isEqual(vehicle, 5, 2, motorcycleSubtypes));
+                boolean validFuels = (vehicle.get(5).get(1).equals("car") &&
+                        isEqual(vehicle, 3, 2, fuels)) ||
+                        (vehicle.get(5).get(1).equals("truck") &&
+                                isEqual(vehicle, 3, 2, truckfuels)) ||
+                        (vehicle.get(5).get(1).equals("motorcycle") &&
+                                vehicle.get(3).get(2).equals("petrol"));
+                String [] wheelTypes = {"winter", "summer", "all-weather"};
+                boolean validWheelTypes = isEqual(vehicle, 6, 2, wheelTypes);
+                boolean wheelNumberCheck =
+                        !vehicle.get(6).get(3).chars().allMatch(Character::isAlphabetic);
+                boolean rightNumberWheels = (vehicle.get(5).get(1).equals(
+                        "car") && vehicle.get(6).get(3).equals("4")) || (vehicle.get(5).get(1).equals("motorcycle")
+                        && vehicle.get(6).get(3).equals("2")) || (vehicle.get(5).get(2).equals("pickup") && vehicle.get(6).get(3).equals("4")) ||
+                        (vehicle.get(5).get(2).equals("pickup") && vehicle.get(6).get(3).equals("6")) ||
+                        (vehicle.get(5).get(2).equals("eighteen wheeler") && vehicle.get(6).get(3).equals("18"));
+                boolean valid =
+                        validColours && validManufacturers && validTypes &&
+                                validSubtypes && validFuels && validWheelTypes &&
+                                wheelNumberCheck && rightNumberWheels;
+                return valid;
+            }
+
+        public static boolean isEqual(ArrayList<ArrayList<String>> vehicle,
+                                      int i, int j, String[] options)
+            {
+                for (String option : options)
+                    {
+                        if (vehicle.get(i).get(j).equals(option)) return true;
+                    }
+                return false;
+            }
+
     }
 
 
